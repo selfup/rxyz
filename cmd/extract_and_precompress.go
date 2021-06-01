6743635f3dd3eb93b747c2bad314f2358cfb9e72
selfup <boudinot.regis@yahoo.com>
2021-06-01 15:42:59 -0600

    use input file to chars - dynamic byte mapping - better run script

--- cmd/extract_and_precompress.go
+++ cmd/extract_and_precompress.go
@@ -8,86 +8,19 @@ import (
 )
 
 func main() {
-	/*
-		ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789./:@#!$*-_+=
-	*/
+	inputCharMap := make(map[string]byte)
 
-	inputCharMap := make(map[string]string)
+	inputChars, icerr := ioutil.ReadFile("chars/input.txt")
 
-	inputCharMap["a"] = "\x00"
-	inputCharMap["A"] = "\x01"
-	inputCharMap["b"] = "\x02"
-	inputCharMap["B"] = "\x03"
-	inputCharMap["c"] = "\x04"
-	inputCharMap["C"] = "\x05"
-	inputCharMap["d"] = "\x06"
-	inputCharMap["D"] = "\x07"
-	inputCharMap["e"] = "\x08"
-	inputCharMap["E"] = "\x09"
-	inputCharMap["f"] = "\x10"
-	inputCharMap["F"] = "\x11"
-	inputCharMap["g"] = "\x12"
-	inputCharMap["G"] = "\x13"
-	inputCharMap["h"] = "\x14"
-	inputCharMap["H"] = "\x15"
-	inputCharMap["i"] = "\x16"
-	inputCharMap["I"] = "\x17"
-	inputCharMap["j"] = "\x18"
-	inputCharMap["J"] = "\x19"
-	inputCharMap["k"] = "\x20"
-	inputCharMap["K"] = "\x21"
-	inputCharMap["l"] = "\x22"
-	inputCharMap["L"] = "\x23"
-	inputCharMap["m"] = "\x24"
-	inputCharMap["M"] = "\x25"
-	inputCharMap["n"] = "\x26"
-	inputCharMap["N"] = "\x27"
-	inputCharMap["o"] = "\x28"
-	inputCharMap["O"] = "\x29"
-	inputCharMap["p"] = "\x30"
-	inputCharMap["P"] = "\x31"
-	inputCharMap["q"] = "\x32"
-	inputCharMap["Q"] = "\x33"
-	inputCharMap["r"] = "\x34"
-	inputCharMap["R"] = "\x35"
-	inputCharMap["s"] = "\x36"
-	inputCharMap["S"] = "\x37"
-	inputCharMap["t"] = "\x38"
-	inputCharMap["T"] = "\x39"
-	inputCharMap["u"] = "\x40"
-	inputCharMap["U"] = "\x41"
-	inputCharMap["v"] = "\x42"
-	inputCharMap["V"] = "\x43"
-	inputCharMap["w"] = "\x44"
-	inputCharMap["W"] = "\x45"
-	inputCharMap["x"] = "\x46"
-	inputCharMap["X"] = "\x47"
-	inputCharMap["y"] = "\x48"
-	inputCharMap["Y"] = "\x49"
-	inputCharMap["z"] = "\x50"
-	inputCharMap["Z"] = "\x51"
-	inputCharMap["."] = "\x52"
-	inputCharMap["/"] = "\x53"
-	inputCharMap[":"] = "\x54"
-	inputCharMap["@"] = "\x55"
-	inputCharMap["#"] = "\x56"
-	inputCharMap["!"] = "\x57"
-	inputCharMap["$"] = "\x58"
-	inputCharMap["*"] = "\x59"
-	inputCharMap["-"] = "\x60"
-	inputCharMap["_"] = "\x61"
-	inputCharMap["+"] = "\x62"
-	inputCharMap["="] = "\x63"
-	inputCharMap["1"] = "\x64"
-	inputCharMap["2"] = "\x65"
-	inputCharMap["3"] = "\x66"
-	inputCharMap["4"] = "\x67"
-	inputCharMap["5"] = "\x68"
-	inputCharMap["6"] = "\x69"
-	inputCharMap["7"] = "\x70"
-	inputCharMap["8"] = "\x71"
-	inputCharMap["9"] = "\x72"
-	inputCharMap["0"] = "\x73"
+	if icerr != nil {
+		log.Fatal(icerr)
+	}
+
+	trimmedInputChars := strings.Trim(string(inputChars), "\n")
+
+	for idx, inputChar := range strings.Split(trimmedInputChars, "") {
+		inputCharMap[inputChar] = byte(idx)
+	}
 
 	file, ferr := ioutil.ReadFile("fixtures/test_data.txt")
 
@@ -100,7 +33,7 @@ func main() {
 	output := ""
 
 	for _, fileChar := range strings.Split(fileChars, "") {
-		output += inputCharMap[fileChar]
+		output += string(inputCharMap[fileChar])
 	}
 
 	filePath := "fixtures/precompressed_data.txt"
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,24 +2,26 @@
 
 set -eou pipefail
 
+mkdir -p tmp
+
 echo '--- building bins'
 
-go build -o tdg cmd/test_data_generator.go
-go build -o tdc cmd/test_data_compressor.go
-go build -o eap cmd/extract_and_precompress.go
-go build -o pc cmd/precompressed_compressor.go
+go build -o tmp/tdg cmd/test_data_generator.go
+go build -o tmp/tdc cmd/test_data_compressor.go
+go build -o tmp/eap cmd/extract_and_precompress.go
+go build -o tmp/pc cmd/precompressed_compressor.go
 
 echo '--- bins built'
 
 echo '--- executing bins'
 
-./tdg && ./tdc && ./eap && ./pc
+./tmp/tdg
+./tmp/tdc
+./tmp/eap
+./tmp/pc
 
 echo '--- removing bins'
 
-rm tdg
-rm tdc
-rm eap
-rm pc
+rm -rf tmp
 
 echo '--- done'
